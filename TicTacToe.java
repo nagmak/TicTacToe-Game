2993e5e084979721b73df314d59aefabce55f591
NAGMA KAPOOR <nagmakapoor@gmail.com>
2016-06-21 02:51:23 -0400

    Improved comments, cleaned up code

--- TicTacToe.java
+++ TicTacToe.java
@@ -10,7 +10,7 @@ public class TicTacToe{
 	private static Random r = new Random();
 	private static Scanner key = new Scanner(System.in);
 	private static final String name2 = "Computer";
-	private static final int ROWS = 3, COLS = 3;
+	private static final int ROWS = 3, COLS = 3, FULL = 9;
 
 	public static void main(String[] args){
 		init_board(); // initializes + displays empty board
@@ -22,6 +22,8 @@ public class TicTacToe{
 		System.out.println(name1 + ", please select your character[X] or [O]: ");
 		char name1_char = key.next().charAt(0);
 		char name2_char = '\0';
+
+		// Error handling for character selection
 		while(name1_char != 'X' && name1_char != 'x' && name1_char !='O' && name1_char != 'o'){
 				System.out.print("Incorrect character. Please enter a character[X] or [O]: ");
 				name1_char = key.next().charAt(0);
@@ -58,17 +60,17 @@ public class TicTacToe{
 			}
 
 			// Checks for Win/Tie
-			if (detect_wintie(name1_char) == true){
+			if (detect_wintie(name1_char)){
 				System.out.println(name1 + ", YOU WIN!");
 				user = 0;
 				break;
 			}
-			if (detect_wintie(name2_char) == true){
+			if (detect_wintie(name2_char)){
 				System.out.println(name2 + " WINS.");
 				user = 0;
 				break;
 			}
-			if (calculate_tie() == true){
+			if (calculate_tie()){
 				System.out.println("It's a TIE.");
 				user = 0;
 				break;
@@ -106,58 +108,15 @@ public class TicTacToe{
 		System.out.print("2| " + board[0][2] + " | " + board[1][2] + " | " + board[2][2] + "\n");
 	}
 
-	// Updates board with user selection
+	// Updates board with human player selection
 	public static void update_board(char name1_char, int row, int col){
 		if (board[row][col] == ' '){
 			board[row][col] = name1_char;
 		}
-
 		display_board(); // display
 	}
 
-	// Winner or Tie detection
-	public static boolean detect_wintie(char select){
-
-		// Checks for 3 in a row/col/diagonal
-		for (int row = 0; row < ROWS; row++){
-			if (board[row][0] == select && board[row][1] == select && board[row][2] == select){
-				return true;
-			}
-		}
-
-		for (int col = 0; col < COLS; col++){
-			if (board[0][col] == select && board[1][col] == select && board[2][col] == select){
-				return true;
-			}
-		}
-
-		if (board[0][0] == select && board[1][1] == select && board[2][2] == select){
-			return true;
-		}
-		if (board[2][0] == select && board[1][1] == select && board[0][2] == select){
-			return true;
-		}
-		return false; // game continues
-	}
-
-	// Calculates scores to detect a tie
-	public static boolean calculate_tie(){
-		int score = 0;
-		for (int i = 0; i < ROWS; i++){
-			for (int j = 0; j < COLS; j++){
-				if (board[i][j] != ' '){
-					score++;
-				}
-			}
-		}
-
-		if(score == 9){
-			return true;
-		}
-
-		return false;
-	}
-	
+	// Updates board with computer selection
 	public static void computer_choice(char name2_char){
 		for (int row = 0; row < ROWS; row++){
 			for (int col = 0; col < COLS; col++){
@@ -167,9 +126,52 @@ public class TicTacToe{
 				}
 			}
 		}
-		return;
 	}
 
+	// Winner or Tie detection
+	public static boolean detect_wintie(char select){
+
+		// Checks for 3 in a row
+		for (int row = 0; row < ROWS; row++){
+			if (board[row][0] == select && board[row][1] == select && board[row][2] == select){
+				return true;
+			}
+		}
+		// Checks for 3 in a col
+		for (int col = 0; col < COLS; col++){
+			if (board[0][col] == select && board[1][col] == select && board[2][col] == select){
+				return true;
+			}
+		}
+		// Checks for 3 in a diagonal
+		if (board[0][0] == select && board[1][1] == select && board[2][2] == select){
+			return true;
+		}
+		if (board[2][0] == select && board[1][1] == select && board[0][2] == select){
+			return true;
+		}
+		return false; // game continues
+	}
+
+	// Decides if there is a tie
+	public static boolean calculate_tie(){
+		int score = 0;
+		for (int i = 0; i < ROWS; i++){
+			for (int j = 0; j < COLS; j++){
+				if (board[i][j] != ' '){
+					score++;
+				}
+			}
+		}
+		// Checks if the board is full
+		if(score == FULL){
+			return true;
+		}
+
+		return false;
+	}
+
+	// Error handling for row/col human player input
 	public static int row_col_choice(int row_col){
 		while(row_col < 0 || row_col > 2){
 			System.out.print("Incorrect input. Please enter a value between 0 and 2: ");
