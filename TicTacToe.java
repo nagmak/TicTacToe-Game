1e29b4e86b20016278f9248d3bbcae9711fe0d73
NAGMA KAPOOR <nagmakapoor@gmail.com>
2016-06-23 16:06:08 -0400

    Updated variable and method names to fit Java conventions

--- TicTacToe.java
+++ TicTacToe.java
@@ -9,34 +9,34 @@ public class TicTacToe{
 	private static int user = 1;
 	private static Random r = new Random();
 	private static Scanner key = new Scanner(System.in);
-	private static final String NAME2 = "Computer";
+	private static final String COMPUTER_NAME = "Computer";
 	private static final int ROWS = 3, COLS = 3, FULL = 9;
 
 	public static void main(String[] args){
-		init_board(); // initializes + displays empty board
+		initializeBoard();
 
 		// Game-play Setup
 		System.out.println("\n-- ULTIMATE TIC TAC TOE --\n");
 		System.out.print("User 1, Enter your Name: ");
 		String name1 = key.nextLine();
 		System.out.println(name1 + ", please select your character[X] or [O]: ");
-		char name1_char = key.next().charAt(0);
-		char name2_char = '\0';
+		char name1Char = key.next().charAt(0);
+		char computerChar = '\0';
 
 		// Error handling for character selection
-		while(name1_char != 'X' && name1_char != 'x' && name1_char !='O' && name1_char != 'o'){
+		while(name1Char != 'X' && name1Char != 'x' && name1Char !='O' && name1Char != 'o'){
 				System.out.print("Incorrect character. Please enter a character[X] or [O]: ");
-				name1_char = key.next().charAt(0);
+				name1Char = key.next().charAt(0);
 		}
 		
 		// Determines the Computer's character
-		if (name1_char == 'X' || name1_char == 'x'){
-			System.out.println(NAME2 + "'s character is now " + 'O');
-			name2_char = 'O';
+		if (name1Char == 'X' || name1Char == 'x'){
+			System.out.println(COMPUTER_NAME + "'s character is now " + 'O');
+			computerChar = 'O';
 		}
-		else if (name1_char == 'o' || name1_char == 'O'){
-			System.out.println(NAME2 + "'s character is now " + 'X');
-			name2_char = 'X';
+		else if (name1Char == 'o' || name1Char == 'O'){
+			System.out.println(COMPUTER_NAME + "'s character is now " + 'X');
+			computerChar = 'X';
 		}
 
 		// Main Program
@@ -45,34 +45,34 @@ public class TicTacToe{
 				System.out.println(name1 + ", it's your turn.");
 				System.out.print("Where do you want to put it? [col (0 - 2)]: ");
 				int col = key.nextInt();
-				col = row_col_choice(col); // col error check
+				col = rowcolErrorCheck(col); // col error check
 
 				System.out.print("[row (0 - 2)]: ");
 				int row = key.nextInt();
-				row = row_col_choice(row); // row error check
-				update_board(name1_char, col, row);
+				row = rowcolErrorCheck(row); // row error check
+				updateBoard(name1Char, col, row);
 				user = 2;
 			}
 			else if (user == 2){
-				System.out.println("\n" + NAME2 + "'s turn.");
-				computer_choice(name2_char);
-				System.out.print("\n" + NAME2 + " has made its move.");
-				display_board();
+				System.out.println("\n" + COMPUTER_NAME + "'s turn.");
+				computerChoice(computerChar);
+				System.out.print("\n" + COMPUTER_NAME + " has made its move.");
+				displayBoard();
 				user = 1;
 			}
 
 			// Checks for Win/Tie
-			if (detect_wintie(name1_char)){
+			if (detectWintie(name1Char)){
 				System.out.println(name1 + ", YOU WIN!");
 				user = 0;
 				break;
 			}
-			if (detect_wintie(name2_char)){
-				System.out.println(NAME2 + " WINS.");
+			if (detectWintie(computerChar)){
+				System.out.println(COMPUTER_NAME + " WINS.");
 				user = 0;
 				break;
 			}
-			if (calculate_tie()){
+			if (calculateTie()){
 				System.out.println("It's a TIE.");
 				user = 0;
 				break;
@@ -81,18 +81,18 @@ public class TicTacToe{
 	}
 
 	// Initial board with blank characters
-	public static void init_board(){
+	public static void initializeBoard(){
 		for(int col = 0; col < COLS; col++){
 			for (int row = 0; row < ROWS; row++){
 				board[col][row] = ' ';
 			}
 		}
 		System.out.println("Welcome to a game of Tic Tac Toe. I hope you're ready.");
-		display_board(); // display
+		displayBoard(); // display
 	}
 
 	// Displays the Tic Tac Toe board
-	public static void display_board(){
+	public static void displayBoard(){
 		System.out.print("\n   0   1   2\n");
 		System.out.print("0| " + board[0][0] + " | " + board[1][0] + " | " + board[2][0] + "\n");
 		System.out.print("  -----------\n");
@@ -102,7 +102,7 @@ public class TicTacToe{
 	}
 
 	// Updates board with human player selection
-	public static void update_board(char name1_char, int col, int row){
+	public static void updateBoard(char name1Char, int col, int row){
 		while(board[col][row] != ' '){
 			System.out.println("Oops. That spot has been taken.");
 			System.out.print("Select a col[0 - 2]: ");
@@ -112,13 +112,13 @@ public class TicTacToe{
 		}
 
 		if (board[col][row] == ' '){
-			board[col][row] = name1_char;
+			board[col][row] = name1Char;
 		}
-		display_board(); // display
+		displayBoard(); // display
 	}
 
 	// Updates board with computer selection
-	public static void computer_choice(char name2_char){
+	public static void computerChoice(char computerChar){
 		int col = r.nextInt(COLS);
 		int row = r.nextInt(ROWS);
 		while(board[row][col] != ' '){
@@ -127,12 +127,12 @@ public class TicTacToe{
 		}
 
 		if (board[col][row] == ' '){
-			board[col][row] = name2_char;
+			board[col][row] = computerChar;
 		}
 	}
 
 	// Winner or Tie detection
-	public static boolean detect_wintie(char select){
+	public static boolean detectWintie(char select){
 
 		// Checks for 3 in a col
 		for (int col = 0; col < COLS; col++){
@@ -157,7 +157,7 @@ public class TicTacToe{
 	}
 
 	// Decides if there is a tie
-	public static boolean calculate_tie(){
+	public static boolean calculateTie(){
 		int score = 0;
 		for (int col = 0; col < COLS; col++){
 			for (int row = 0; row < ROWS; row++){
@@ -175,7 +175,7 @@ public class TicTacToe{
 	}
 
 	// Error handling for row/col human player input
-	public static int row_col_choice(int row_col){
+	public static int rowcolErrorCheck(int row_col){
 		while(row_col < 0 || row_col > 2){
 			System.out.print("Incorrect input. Please enter a value between 0 and 2: ");
 			row_col = key.nextInt();
